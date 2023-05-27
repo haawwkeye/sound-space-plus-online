81d06888c32ea05edc2de7ff05a580e37c64cc82
jayden <46307433+krmeet@users.noreply.github.com>
2023-05-27 16:39:46 +0100

    20 -> 50

--- src/routes/api/users/list/+server.ts
+++ src/routes/api/users/list/+server.ts
@@ -2,7 +2,7 @@ import prisma from "$lib/prisma";
 import { json } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
 
-const pagesize = 20
+const pagesize = 50
 
 export const GET: RequestHandler = async (event) => {
 	var page = Number(event.url.searchParams.get("page") ?? 0)
