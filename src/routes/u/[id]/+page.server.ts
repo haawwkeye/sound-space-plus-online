d8b08e3bdc004c01734c5af7c3ed03d368199537
jayden <46307433+krmeet@users.noreply.github.com>
2023-05-30 00:40:56 +0100

    ssr profiles

---
+++ src/routes/u/[id]/+page.server.ts
@@ -0,0 +1,14 @@
+import prisma from "$lib/prisma";
+import { error } from "@sveltejs/kit";
+import type { PageServerLoad } from "./$types";
+
+export const load: PageServerLoad = async ({ params }) => {
+	var id = Number(params.id ?? 0);
+	var user = await prisma.user.findUnique({
+		where: {
+			id: id
+		}
+	})
+	if (!user) throw error(404)
+	return { user: user }
+}
--- src/routes/u/[id]/+page.svelte
+++ src/routes/u/[id]/+page.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
 	import Title from "../../Title.svelte";
-	import { page } from "$app/stores";
-	import type internal from "stream";
-	import { onMount } from "svelte";
+	import type { PageServerData } from "./$types";
 
 	type User = {
 		id: number;
@@ -12,27 +10,17 @@
 		role: string;
 		dateCreated: Date;
 	};
-	let user: User;
-	let userExists: boolean = true;
 
-	async function getUser() {
-		var id = $page.params.id;
-		var result = await fetch(`/api/users/profile?id=${id}`);
-		if (!result.ok) {
-			userExists = false;
-			return;
-		}
-		user = await result.json();
-	}
-	onMount(getUser);
+	export let data: PageServerData;
+
+	const user = data.user;
 </script>
 
-{#if !userExists}
-	<h1>404</h1>
-	<h2>This user doesn't exist</h2>
-{:else if user != null}
-	<Title title={user.name} />
-	<h2>{user.name}</h2>
-	{#if user.verified} <span>This user is verified</span><br /> {/if}
-	{#if user.role == "ADMIN"} <span>This user is an admin</span><br /> {/if}
-{/if}
+<sveltekit:head>
+	<meta property="og:title" content={user.name} />
+</sveltekit:head>
+
+<Title title={user.name} />
+<h2>{user.name}</h2>
+{#if user.verified} <span>This user is verified</span><br /> {/if}
+{#if user.role == "ADMIN"} <span>This user is an admin</span><br /> {/if}
