37844d91813a26382c2d13ebbe138deda15509dd
jayden <46307433+krmeet@users.noreply.github.com>
2023-05-30 02:56:52 +0100

    fix logout crash

--- src/routes/logout/+server.ts
+++ src/routes/logout/+server.ts
@@ -2,6 +2,6 @@ import { redirect, type RequestHandler } from "@sveltejs/kit";
 import { POST } from "../api/auth/logout/+server";
 
 export const GET: RequestHandler = async (event) => {
-	POST(event)
+	await POST(event)
 	throw redirect(302, "/")
 }
