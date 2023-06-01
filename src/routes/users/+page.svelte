645a3133f5998ed14731fcf78c346e169cb2b48b
jayden <46307433+krmeet@users.noreply.github.com>
2023-06-02 00:54:56 +0100

    both sides

--- src/routes/users/+page.svelte
+++ src/routes/users/+page.svelte
@@ -9,6 +9,7 @@
 
 <Title title="Users" />
 <h2>Users</h2>
+<p>{$page.data.count} total users</p>
 <div class="paginator">
 	<span>{$page.data.currentPage}/{$page.data.noPages} pages</span>
 	<br />
@@ -36,6 +37,16 @@
 	{/each}
 </ul>
 <p>{$page.data.count} total users</p>
+<div class="paginator">
+	<span>{$page.data.currentPage}/{$page.data.noPages} pages</span>
+	<br />
+	<a href="?pg=1"> &lt;&lt;</a>
+	<a href="?pg={Math.max($page.data.currentPage - 1, 1)}"> &lt;</a>
+	<a href="?pg={Math.min($page.data.currentPage + 1, $page.data.noPages)}">
+		&gt;</a
+	>
+	<a href="?pg={$page.data.noPages}"> &gt;&gt;</a>
+</div>
 
 <style>
 	.paginator {
