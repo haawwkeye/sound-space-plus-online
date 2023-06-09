fb0183a0d9ade192fca178007c7293464fb2f12d
jayden <46307433+krmeet@users.noreply.github.com>
2023-06-09 21:44:27 +0100

    add demo to website

---
+++ src/routes/demo/[file]/+server.ts
@@ -0,0 +1,17 @@
+import { promises } from "fs";
+import type { RequestHandler } from "@sveltejs/kit";
+
+export const GET: RequestHandler = async (event) => {
+	var response = new Response(
+		await promises.readFile(`static/_demo/${event.params.file}`)
+	)
+	var extension = event.params.file?.split(".").pop()
+	var mime = `text/${extension}`
+	if (extension == "js")
+		mime = "application/javascript"
+	response.headers.append("Content-Type", mime)
+	response.headers.append("Cross-Origin-Opener-Policy", "same-origin")
+	response.headers.append("Cross-Origin-Embedder-Policy", "require-corp")
+	console.log(event.params.file)
+	return response
+}
