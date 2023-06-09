27dcfd2c68bdc0294e36b73da645e55a0f6f3428
jayden <46307433+krmeet@users.noreply.github.com>
2023-06-09 23:20:57 +0100

    dont disable that

--- src/routes/demo/+page.server.ts
+++ src/routes/demo/+page.server.ts
@@ -1,8 +1,8 @@
-// import type { PageServerLoad } from "./$types";
+import type { PageServerLoad } from "./$types";
 
-// export const load: PageServerLoad = ({ setHeaders }) => {
-// 	setHeaders({
-// 		"Cross-Origin-Opener-Policy": "same-origin",
-// 		"Cross-Origin-Embedder-Policy": "require-corp"
-// 	})
-// }
+export const load: PageServerLoad = ({ setHeaders }) => {
+	setHeaders({
+		"Cross-Origin-Opener-Policy": "same-origin",
+		"Cross-Origin-Embedder-Policy": "require-corp"
+	})
+}
